88389ac37dbe8619476fc3e5d75cca2a067d1710
Hassanul50 <hasanakib27@gmail.com>
2022-08-25 01:01:22 +0600

    push first time

---
+++ FoodOrder.Solution/FoodOrder.App/Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrder.App.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
